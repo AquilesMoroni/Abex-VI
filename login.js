3115f5a127c030de20d0344f474fc6e54fc3d426
gabrielcallai <gabriel.callai@hotmail.com>
2025-04-03 21:09:33 -0300

    versaoatualizada

--- login.js
+++ login.js
@@ -9,11 +9,13 @@ document.addEventListener('DOMContentLoaded', function () {
             const senha = document.getElementById('senha').value;
 
             // Recupera os dados do usuário do Local Storage
+            console.log(localStorage.getItem('usuario'));
             const usuarioSalvo = JSON.parse(localStorage.getItem('usuario'));
 
             if (usuarioSalvo && email === usuarioSalvo.email && senha === usuarioSalvo.senha) {
                 alert('Login bem-sucedido! Bem-vindo, ' + usuarioSalvo.nome);
-                // Redirecionar para a página principal ou dashboard
+                
+                // Redirecionar para a página dashboard.html
                 window.location.href = 'dashboard.html';
             } else {
                 alert('Erro: E-mail ou senha incorretos.');
